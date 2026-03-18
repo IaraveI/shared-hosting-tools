f10e2494a2ccea446c59c0dd6a8a198d46117bb2
Viktor Szépe <viktor@szepe.net>
2026-03-18 07:52:23 +0100

    Add yoast-check.php

---
+++ yoast-check.php
@@ -0,0 +1,20 @@
+<?php
+
+// wp eval-file yoast-check.php
+// wp plugin list --fields=name,version | grep -i 'wordpress-seo'
+
+require_once ABSPATH . 'wp-admin/includes/file.php';
+require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+
+global $wp_filesystem;
+WP_Filesystem();
+
+$r = (new \Yoast\WP\SEO\Integrations\Admin\Check_Required_Version())->check_required_version(
+    WP_PLUGIN_DIR . '/wordpress-seo-premium/',
+    null,
+    new Plugin_Upgrader()
+);
+
+if (is_wp_error($r)) {
+    WP_CLI::error($r->get_error_message());
+}
